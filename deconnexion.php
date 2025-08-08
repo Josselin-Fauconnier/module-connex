11c5de9a4a5c1883a55911a527b77dd00b192213
Josselin-Fauconnier <josselin.fauconnier@laplateforme.io>
2025-08-08 11:24:41 +0200

    amelioration deconnexion

--- deconnexion.php
+++ deconnexion.php
@@ -2,6 +2,9 @@
 session_start();
 if(session_status()===PHP_SESSION_ACTIVE){
     $_SESSION=array();
+    if (isset($_COOKIE[session_name()])){
+        setcookie(session_name(),'',time()-5400,'/');
+    }
     session_destroy();
 }
 
